31d7b528292ddd85088ac00354c2a3caa114d10e
fairoos <fairoosnm123@gmail.com>
2019-02-28 10:02:23 +0530

    Print summery in article and source code for Youtube video.

--- demo.py
+++ demo.py
@@ -8,7 +8,13 @@ source_code = requests.get(url).text
 soup = BeautifulSoup(source_code, 'lxml')
 
 article =  soup.find("article")
-#print(article.prettify())
+print(article.prettify())
 
-headline = article.h2.a.text
-print(headline)
+#headline = article.h2.a.text
+#print(headline)
+
+summery = article.find('div',class_='entry-content').p.text
+print(summery)
+
+vid_src = article.find('iframe',class_='youtube-player')
+print(vid_src)
