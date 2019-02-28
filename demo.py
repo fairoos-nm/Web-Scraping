e052bb3bb08d875cda4096262bc80819d724ec02
fairoos <fairoosnm123@gmail.com>
2019-02-28 10:26:35 +0530

    Print head_line summery and embeded youtube link in all the articles in the webpage

--- demo.py
+++ demo.py
@@ -7,14 +7,17 @@ source_code = requests.get(url).text
 
 soup = BeautifulSoup(source_code, 'lxml')
 
-article =  soup.find("article")
-print(article.prettify())
+for article in soup.find_all("article"):
+    headline = article.h2.a.text
+    print(headline)
+    
+    summery = article.find('div',class_='entry-content').p.text
+    print(summery )
 
-#headline = article.h2.a.text
-#print(headline)
+    vid_src = article.find('iframe',class_='youtube-player')['src']
+    vid_id = vid_src.split('/')[4]
+    vid_id = vid_id.split('?')[0]
+    youtube_link = f"https://youtube.com/watch?v={vid_id}"
+    print(youtube_link)
 
-summery = article.find('div',class_='entry-content').p.text
-print(summery)
-
-vid_src = article.find('iframe',class_='youtube-player')
-print(vid_src)
+    print()
