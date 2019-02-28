7f19b8b8439d1cb90312d9a65a38212344a0e4d5
fairoos <fairoosnm123@gmail.com>
2019-02-28 09:10:49 +0530

    1- Changed: beautifulsoup to BeautifulSoup in import module because it is case sensitive. 2- Load a website and print a particular portion 'article' in text format.

--- demo.py
+++ demo.py
@@ -1,3 +1,11 @@
 import requests
-from bs4 import beautifulSoup
+from bs4 import BeautifulSoup
 
+url = "https://coreyms.com/"
+
+source_code = requests.get(url).text
+
+soup = BeautifulSoup(source_code, 'lxml')
+
+article =  soup.find("article")
+print(article)
